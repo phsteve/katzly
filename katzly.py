0bf2efe8bb479d73be38352cdd22ebd3305ac846
Steve Katz <katz.stephenj@gmail.com>
2013-11-01 17:35:29 -0400

    fix last commit

--- katzly.py
+++ katzly.py
@@ -3,8 +3,6 @@ import random, string
 from flask import Flask, render_template, send_from_directory, redirect, request
 
 app = Flask(__name__)
-# app.debug = True
-# app.run()
 
 redirects = {}
 
@@ -18,7 +16,6 @@ def encode_url(url):
 
 @app.route('/')
 def index():
-    #print "hello"
     return render_template('index.html')
 
 @app.route('/shortened', methods = ['GET', 'POST'])
@@ -26,19 +23,15 @@ def shortened():
     if request.method == 'POST':
         url = request.form['url']
         encoding = encode_url(url) 
-    #if method is get/post
     return render_template('shortened.html', url=url, encoding=encoding)
-    # Your shortened url for (url) is (short)
 
 @app.route('/<encoding>')
 def redir(encoding):
-    #print redirects[encoding]
     return redirect(redirects[encoding])
 
-# @app.route('/redirects')
-# def show_redirects():
-
-
+@app.route('/encodings')
+def encodings():
+    return render_template('encodings.html', redirects=redirects)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
