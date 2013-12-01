4d147d9109a394fe61efaba4de8a1646bb0b095d
Steve Katz <katz.stephenj@gmail.com>
2013-12-01 13:22:14 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+katzly
+--------------
+
+A link shortener I wrote to get more familiar with web development and Flask.
--- katzly.py
+++ katzly.py
@@ -1,22 +1,46 @@
 import os
 import random, string
+from urlparse import urlparse
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired, Required, URL
 from flask import Flask, render_template, send_from_directory, redirect, request
 
 app = Flask(__name__)
+app.config.from_object('config')
+#from app import views
+
+class URLForm(Form):
+    url = StringField('url', validators = [DataRequired(), Required(message="Please enter a URL."), URL(message="Please enter a valid URL.")])
 
 redirects = {}
 
 def encode_url(url):
     encoding = ''.join(random.choice(string.lowercase) for _ in range(6))
-    while encoding in redirects:
+    while encoding in redirects: #in case there's repeats (1/308915776 chance!)
         #D.R.Y
         encoding = ''.join(random.choice(string.lowercase) for _ in range(6))
     redirects[encoding] = url
     return encoding
 
-@app.route('/')
+def parse_url(url):
+    u = urlparse(url)
+    if not u.scheme:
+        url = 'http://' + url
+    return url
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = URLForm(request.form)
+    if form.validate_on_submit():
+        return redirect('/shortened')
+    # if request.method == 'GET':
+    #     return render_template('index.html')
+    # else:
+    #     url = request.form['url']
+    #     if url:
+    return render_template('index.html', form=form)
+
 
 @app.route('/shortened', methods = ['GET', 'POST'])
 def shortened():
